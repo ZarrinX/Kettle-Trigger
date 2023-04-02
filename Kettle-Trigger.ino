ae80cb993471ddb30a6c3678a72015b8c0623854
Zachary Rice <emperor_zarrin@yahoo.com>
2023-04-01 18:51:54 -0700

    updated filesnames, added serial debugging

--- Kettle-Trigger.ino
+++ Kettle-Trigger.ino
@@ -1,27 +1,27 @@
-//microSD Interface
+// microSD Interface
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 #include <SPI.h>
 #include "SdFat.h" //http://librarymanager/All#SdFat_exFat by Bill Greiman. We currently use v2.1.2
-
 const int pin_microSD_CS = 9; //Default pin for the SparkFun MP3 Shield
 SdFat sd;
-//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-//MP3 Shield Interface
+// MP3 Shield Interface
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 #include "vs1053_SdFat.h" //http://librarymanager/All#vs1053_sdFat by mpflaga
-
 vs1053 MP3player; 
-//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-//Current monitor
+// Current monitor
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 #include "EmonLib.h"             // Include Emon Library
-
+double Irms;                     // Store current value
 EnergyMonitor emon1;             // Create an instance
 
+// Sketch Setup
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 void setup()
 {
+  Serial.begin(9600); //Use serial for debugging
+
   if (sd.begin(pin_microSD_CS) == false)
   {
     Serial.println(F("SD init failed. Is card present? Formatted? Freezing..."));
@@ -29,16 +29,23 @@ void setup()
   }
 
   MP3player.begin();
-  MP3player.setVolume(10, 10); //(Left, Right) 40 is pretty good for ear buds
+  MP3player.setVolume(10, 10); //(Left, Right) 40 is pretty good for ear buds, 10 for speakers
 
   emon1.current(1, 111.1);             // Current: input pin, calibration.
-}
+  Irms = emon1.calcIrms(1480);
 
+  //Give the calcIrms time to settle to base
+  while(Irms > 5){
+    Serial.println(Irms);
+    delay(100);
+    Irms = emon1.calcIrms(1480);
+  }
+}
 void loop()
 {
-  double Irms = emon1.calcIrms(1480);
-  Serial.println(Irms);
+  Irms = emon1.calcIrms(1480);
   if(Irms > 10.0) {
+    Serial.println("Bean! Beans! Beans!");
     MP3player.playMP3("beans.mp3");
     delay(360000);
   }
